b82c48c2651525fba46125cb9bcd429d85266279
jasonbasden <54333628+jasonbasden@users.noreply.github.com>
2020-12-01 21:24:44 -0600

    Update urlCoder.py

--- urlCoder.py
+++ urlCoder.py
@@ -1,7 +1,7 @@
-#urlCoder 
-#**Version 1.0.0**
+#urlCoder
+#**Version 1.0.1**
 #Copyright (c) 2020 Jason Basden
-#Licensed under the MIT License 
+#Licensed under the MIT License
 
 
 import urllib.parse
@@ -9,16 +9,17 @@ import urllib.parse
 userAnswer = "yes"
 useranswer = "yes"
 
-while userAnswer == "yes": 
+while userAnswer == "yes":
     userInput = input("Do you want to URL Encode or URL Decode?: ").lower().strip()
-    
+
     if(userInput == "url encode" or userInput == "encode" or userInput == "urlencode"):
-       
+
         while True:
             userEncoding = input("\nHow would you like to encode spaces? Enter %20 or + : ").lower()
             if(userEncoding == "%20"):
                 userString = input("\nEnter your string: ")
                 print(urllib.parse.quote(userString))
+
                 break
             elif(userEncoding == "+"):
                 userString = input("\nEnter your string: ")
@@ -27,7 +28,7 @@ while userAnswer == "yes":
             elif(userEncoding == ""):
                 print("Error! Enter '%20', '+' or 'quit'.")
             elif(userEncoding == "quit"):
-                break 
+                break
             else:
                 print("Error!")
                 useranswer = input("Would you like to try Encoding again?: ")
@@ -38,11 +39,13 @@ while userAnswer == "yes":
                     break
     elif(userInput == "url decode" or userInput == "decode" or userInput == "urldecode"):
         userString = input("\nEnter your string: ")
-        print(urllib.parse.unquote_plus(userString))
-    
+        while("%" in userString or "+" in userString):
+            decodedString = urllib.parse.unquote_plus(userString)
+            print(decodedString)
+            userString = decodedString
     elif(len(userInput) < 1):
         print("Error! Enter 'Encode' or 'Decode'.")
-    
+
     else:
         print("Error!")
 
